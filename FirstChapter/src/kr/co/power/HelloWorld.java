bcb7450f201e4d7b8b95f34581c1da2d3f89ead1
SangKil Park <ht1911@gmail.com>
2015-12-25 22:26:08 +0900

    First Commit

---
+++ FirstChapter/src/kr/co/power/HelloWorld.java
@@ -0,0 +1,10 @@
+package kr.co.power;
+
+/**
+ * Created by netive on 2015. 12. 25..
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
