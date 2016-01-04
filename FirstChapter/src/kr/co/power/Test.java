779d5660d8bd51eb0f6ea455a5750d8a748d00d4
SangKil Park <ht1911@gmail.com>
2016-01-04 18:33:17 +0900

    Third Commit

--- FirstChapter/src/kr/co/power/Test.java
+++ FirstChapter/src/kr/co/power/Test.java
@@ -7,6 +7,10 @@ public class Test {
     public static void main(String[] args) {
         //System.out.println("test");
 
+        int size = 0;
+
+        System.out.println(--size);
+
     }
 
 }
