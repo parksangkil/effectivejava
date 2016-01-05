01d9fb93c1f4d19ad3f82192beace7772bf25b8f
SangKil Park <ht1911@gmail.com>
2016-01-05 21:18:47 +0900

    Building a RESTful Web Service

---
+++ MavenProject/src/main/java/hello/Application.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by netive on 2016. 1. 5..
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
